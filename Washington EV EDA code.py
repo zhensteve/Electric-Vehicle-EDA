bba64cde2c99bac3e18af1b75155da177506a37a
zhensteve <zhenebill@gmail.com>
2024-07-18 21:01:08 -0400

    Committing Revision 7/18

--- Washington EV EDA code.py
+++ Washington EV EDA code.py
@@ -10,14 +10,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import scipy.stats as st
-import os 
 
-ev_vehicle = pd.read_csv("C:/Users/kaizh/Downloads/Electric_Vehicle_Population_Data.csv")
+
+ev_vehicle = pd.read_csv("C:/Users/xiaom/OneDrive/Desktop/Github Folder/Electric-Vehicle-EDA/Electric_Vehicle_Population_Data.csv")
 
 ev_vehicle.columns
 
 ev_vehicle.duplicated().sum()
+#Overall no Duplicates in the Electric Vehicle Dataset
 
+#Checking if the EV dataset have any missing values(for each variable)
 ev_vehicle.isna().sum()
 
 ev_vehicle[ev_vehicle['County'].isna()]
@@ -29,7 +31,7 @@ cnt_legis_district = ev_vehicle['Legislative District'].value_counts() \
                                   .rename(columns = {'index':'Legislative District', 
                                                      'Legislative District': 'N'})
 
-
+#Subsetted out whole dataset to only vehicles registered to the state of Washington
 ev_subset = ev_vehicle.loc[ev_vehicle["State"] == "WA", ["County", "City", "Model Year", "Make", 
                                                       "Model", "Electric Vehicle Type", 
                                                       "Clean Alternative Fuel Vehicle (CAFV) Eligibility", 
@@ -40,69 +42,60 @@ ev_subset = ev_vehicle.loc[ev_vehicle["State"] == "WA", ["County", "City", "Mode
                              'Base MSRP':"Base MSRP", 
                              "Model Year": "Model_Year"}).copy()
 
-ev_subset.columns
-
 ev_subset.info()
 
-
+# Count of registered EV by county and city
 city_county_count = ev_subset[["County", "City"]] \
     .value_counts() \
     .reset_index() \
     .rename(columns = {0:"N"}) \
     .sort_values(by = "N", ascending = False)
 
-#city_county_count
-
-
-'''
-Q1. which county + city have the most EV?
-Q2. Which EV (Model + Make + Model Year) is most popular?
-Q2B. See if popular EV is popular because the practically, or brand
-Q3. Is there a difference in preference EV state between top EV states and non ev states
-'''
-
-Seems like the top ten counties accounts for about 91% of the total EV ownership in the State of Washington (2020 Census). Amongst the 
-all counties, Kings county accounts for about 51% of the EV owernship. Based on this finding, we need to if the count of EV differs between 
-county? (Potential intermediate questions - what are the most popular in the top 10 states? ) 
-
-
+#Count of registered EV by County
 county_count = ev_subset["County"] \
     .value_counts(normalize = True) \
     .reset_index() \
     .rename(columns = {"index": "County",
                        "County": "N"})
 
-
-#county_count
-
-#county_count["N"].head(10).sum()
-
+# Subsetting for the top 10 counties w/ EV
 top_ten_county = county_count.head(10).copy()
-
+'''
+Q1. which county + city have the most EV? Amongst top 10 - Which EV (Model + Make + Model Year) is most popular?
+Q2. How does those EV (from q1) compare the ones that did not make to the list?
+Q3. Is there a difference in preference EV state between top EV states and non ev states?
+Limitations of the exploratory data analysis
+'''
+'''
+Seems like the top ten counties accounts for about 91% of the total EV ownership in the State of Washington (2020 Census). Amongst the 
+all counties, Kings county accounts for about 51% of the EV owernship. Based on this finding, we need to if the count of EV differs between 
+county? (Potential intermediate questions - what are the most popular in the top 10 states? ) 
+'''
 
 car_variable = ['County','Model_Year', 'Make', 'Model', 'EV_Type', 'CAFV', 'Electric_Range', 'Base MSRP']
+#Looking at the EV only associated w/ the top 10 county
 car_detail_df = ev_subset.loc[ev_subset["County"].isin(top_ten_county["County"]), car_variable].copy()
 
 
+# Count of EV car make by county (numerator)
 cars_count_by_county = car_detail_df.groupby(by = ["County", "Make", "Model"], as_index = False)["County"].agg(["count"]) \
              .reset_index() \
              .sort_values(by = ["County", "count"], ascending = False)
 
-
+# Count of all ev by county (Denominator)
 total_ev_top_10 = cars_count_by_county.groupby(by = "County", as_index = False)["count"] \
                     .agg(["sum"]) \
                     .reset_index() \
                     .rename(columns = {"sum":"Total"}) \
                     .drop_duplicates(subset = "County").copy()
 
-
-
+#Getting the top ev per each county
 top_cars_by_county = (cars_count_by_county.drop_duplicates(subset = ["County"], keep = "first")
                                          .sort_values(by = "count", ascending = False)
                                          .reset_index(drop = True)
                                          .copy())
 
-
+#Joining numerator dataset w/ denominator dataset to get the % of the most popular EV accounts for each county
 county_car_join = (top_cars_by_county.merge(total_ev_top_10, how = "left", 
                         left_on = "County", 
                         right_on = "County", 
